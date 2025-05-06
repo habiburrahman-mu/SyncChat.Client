a960a71b70e82d276e73cef24a4690408f610c37
Habibur Rahman <habiburrahman.mu@gmail.com>
2025-05-07 00:37:16 +0600

    feat(chat): integrate Angular Material components and Tailwind CSS
    - Added Angular Material CDK and Material dependencies to package.json.
    - Updated chat.component.html to use mat-drawer and mat-accordion for improved layout and functionality.
    - Modified chat.component.ts to include MatExpansionModule and MatSidenavModule, and implemented signal for panel state management.
    - Included Google Fonts and Material Icons in index.html for better styling.
    - Configured Tailwind CSS in styles.scss and added a PostCSS configuration file.

--- src/app/features/chat/components/chat/chat.component.ts
+++ src/app/features/chat/components/chat/chat.component.ts
@@ -1,11 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import {MatExpansionModule} from '@angular/material/expansion';
+import {MatSidenavModule} from '@angular/material/sidenav';
 
 @Component({
   selector: 'chat-chat',
-  imports: [],
+  imports: [MatExpansionModule, MatSidenavModule],
   templateUrl: './chat.component.html',
-  styleUrl: './chat.component.scss'
+  styleUrl: './chat.component.scss',
 })
 export class ChatComponent {
-
+  readonly panelOpenState = signal(false);
 }
