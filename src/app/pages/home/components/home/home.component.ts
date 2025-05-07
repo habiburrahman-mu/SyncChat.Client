e9a79e931eaa2b7ae7d61075fe6f0f15ce7d1a47
Habibur Rahman <habiburrahman.mu@gmail.com>
2025-05-07 19:05:02 +0600

    feat(home): restructure home page components; implement header, hero, features, about, testimonials, and footer sections

--- src/app/pages/home/components/home/home.component.ts
+++ src/app/pages/home/components/home/home.component.ts
@@ -1,8 +1,14 @@
 import { Component } from '@angular/core';
+import { HomeHeaderComponent } from "../home-header/home-header.component";
+import { HomeHeroComponent } from "../home-hero/home-hero.component";
+import { HomeFeaturesComponent } from "../home-features/home-features.component";
+import { HomeAboutComponent } from "../home-about/home-about.component";
+import { HomeTestimonialsComponent } from "../home-testimonials/home-testimonials.component";
+import { HomeFooterComponent } from "../home-footer/home-footer.component";
 
 @Component({
   selector: 'chat-home',
-  imports: [],
+  imports: [HomeHeaderComponent, HomeHeroComponent, HomeFeaturesComponent, HomeAboutComponent, HomeTestimonialsComponent, HomeFooterComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
