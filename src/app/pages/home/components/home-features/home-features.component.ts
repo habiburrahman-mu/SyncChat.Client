f0de060c0f10e396783e73a09f12f4f9d3a89122
Habibur Rahman <habiburrahman.mu@gmail.com>
2025-05-07 18:58:59 +0600

    feat(home): redesign home page layout and components; add header, footer, hero, features, testimonials, and about sections

---
+++ src/app/pages/home/components/home-features/home-features.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'chat-home-features',
+  imports: [],
+  templateUrl: './home-features.component.html',
+  styleUrl: './home-features.component.scss'
+})
+export class HomeFeaturesComponent {
+
+}
---
+++ src/app/pages/home/components/home-hero/home-hero.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'chat-home-hero',
+  imports: [],
+  templateUrl: './home-hero.component.html',
+  styleUrl: './home-hero.component.scss'
+})
+export class HomeHeroComponent {
+
+}
