e52be6666ff0f9b2229ef763883f5dab7bbff04e
Habibur Rahman <habiburrahman.mu@gmail.com>
2025-06-17 00:01:27 +0600

    refactor(routes): use constants for feature route paths

--- src/app/features/features.routes.ts
+++ src/app/features/features.routes.ts
@@ -1,6 +1,11 @@
 import { Routes } from "@angular/router";
 
+export const FEATURE_ROUTE_PATH = {
+  Auth: 'auth',
+  Chat: 'chat'
+}
+
 export const FEATURE_ROUTES: Routes = [
-  { path: 'auth', loadChildren: () => import('@features/auth/auth.routes').then(r => r.AUTH_ROUTES) },
-  { path: 'chat', loadChildren: () => import('@features/chat/chat.routes').then(r => r.CHAT_ROUTES) },
+  { path: FEATURE_ROUTE_PATH.Auth, loadChildren: () => import('@features/auth/auth.routes').then(r => r.AUTH_ROUTES) },
+  { path: FEATURE_ROUTE_PATH.Chat, loadChildren: () => import('@features/chat/chat.routes').then(r => r.CHAT_ROUTES) },
 ];
