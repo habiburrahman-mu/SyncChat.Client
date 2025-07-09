711d16ae383ac9d9201a8862b1999b2c14307025
Habibur Rahman <habiburrahman.mu@gmail.com>
2025-07-09 21:33:15 +0600

    feat(enums): add ConversationType and MessageType enums for conversation and message handling

--- src/app/core/enums/index.ts
+++ src/app/core/enums/index.ts
@@ -1,2 +1,3 @@
 export * from './local-storage-key.enum';
-export * from './conversation-type';
+export * from './conversation-type.enum';
+export * from './message-type.enum';
---
+++ src/app/core/enums/message-type.enum.ts
@@ -0,0 +1,6 @@
+export enum MessageType {
+  Text = 'Text',
+  Image = 'Image',
+  Video = 'Video',
+  File = 'File'
+}
