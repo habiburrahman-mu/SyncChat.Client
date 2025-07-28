1330c2860b206a9686be7273fa063bcda5d39073
Habibur Rahman <habiburrahman.mu@gmail.com>
2025-07-28 23:57:57 +0600

    feat(home-header): replace button with router link for navigation to chat

--- src/app/pages/home/components/home-header/home-header.component.ts
+++ src/app/pages/home/components/home-header/home-header.component.ts
@@ -1,8 +1,11 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'chat-home-header',
-  imports: [],
+  imports: [CommonModule, RouterModule, MatButtonModule],
   templateUrl: './home-header.component.html',
   styleUrl: './home-header.component.scss'
 })
