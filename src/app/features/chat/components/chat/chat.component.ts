526ad4e06b7f8a679b81888d7574e412c9c54a1f
Habibur Rahman <habiburrahman.mu@gmail.com>
2025-08-23 21:32:57 +0600

    feat(chat): wrap ChatDetailPanel in a div and update chat component structure for improved layout

--- src/app/features/chat/components/chat/chat.component.ts
+++ src/app/features/chat/components/chat/chat.component.ts
@@ -2,13 +2,17 @@ import { ChangeDetectionStrategy, Component, inject, OnDestroy, OnInit } from '@
 import { ChatSidebarComponent } from "../chat-sidebar/chat-sidebar.component";
 import { ChatStateService } from '@features/chat/services';
 import { ChatThreadComponent } from '../chat-thread/chat-thread.component';
+import { ChatDetailPanelComponent } from "../chat-detail-panel/chat-detail-panel.component";
+import { MatSidenavModule } from '@angular/material/sidenav';
 
 @Component({
   selector: 'chat-chat',
   imports: [
     ChatSidebarComponent,
     ChatThreadComponent,
-  ],
+    ChatDetailPanelComponent,
+    MatSidenavModule
+],
   templateUrl: './chat.component.html',
   styleUrl: './chat.component.scss',
 })
