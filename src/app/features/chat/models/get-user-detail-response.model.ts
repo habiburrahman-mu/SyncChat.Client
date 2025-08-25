7d10342a41d2aa81b305415e8ef09fbb6cda1660
Habibur Rahman <habiburrahman.mu@gmail.com>
2025-08-26 00:28:43 +0600

    fix(user): correct userID casing and update status type to use UserStatus enum

--- src/app/features/chat/models/get-user-detail-response.model.ts
+++ src/app/features/chat/models/get-user-detail-response.model.ts
@@ -1,12 +1,14 @@
+import { UserStatus } from "@core/enums";
+
 export interface GetUserDetailResponse {
-  userId: number;
+  userID: number;
   uuid: string;
   userName: string;
   name: string;
   email: string;
   phone?: string;
   profile: any; // Consider using a more specific type if you know the JSON structure
-  status: 'Active' | 'Inactive' | 'Suspended' | 'Deleted'; // Adjust based on your UserStatus enum
+  status: UserStatus; // Adjust based on your UserStatus enum
   lastActive?: string; // ISO date string
   createdAt: string;   // ISO date string
   isVerified: boolean;
