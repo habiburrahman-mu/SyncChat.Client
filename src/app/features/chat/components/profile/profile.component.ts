e22c79b9bc6955780c9de8a50505f0d92e56148c
Habibur Rahman <habiburrahman.mu@gmail.com>
2025-08-27 00:37:23 +0600

    feat(profile): enhance profile component layout and add user information display

--- src/app/features/chat/components/profile/profile.component.ts
+++ src/app/features/chat/components/profile/profile.component.ts
@@ -1,12 +1,14 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatDialogModule } from '@angular/material/dialog';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'chat-profile',
   imports: [
     MatDialogModule,
-    MatButtonModule
+    MatButtonModule,
+    MatIconModule
   ],
   templateUrl: './profile.component.html',
   styleUrl: './profile.component.scss',
