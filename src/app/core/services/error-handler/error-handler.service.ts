c40e1d670684a7b8d2995f7fbb36ff11d88966a4
Habibur Rahman <habiburrahman.mu@gmail.com>
2025-12-07 23:00:38 +0600

    fix(error-handler): improve client error handling in ErrorHandlerService

--- src/app/core/services/error-handler/error-handler.service.ts
+++ src/app/core/services/error-handler/error-handler.service.ts
@@ -4,7 +4,7 @@ import { ToasterService } from '../toaster/toaster.service';
 
 @Injectable({ providedIn: 'root' })
 export class ErrorHandlerService {
-  constructor(private toaster: ToasterService) {}
+  constructor(private toaster: ToasterService) { }
 
   handle(error: HttpErrorResponse) {
     let message = 'An unexpected error occurred!';
@@ -13,8 +13,15 @@ export class ErrorHandlerService {
       this.toaster.error('Network Error', 'Unable to connect to the server. Please check your connection.');
     }
     else if (error.status >= 400 && error.status < 500) {
-      message = error.error?.detail || 'A client error occurred.';
-      this.toaster.warning(`Client Error: ${error.status}`, message);
+
+      if (error.error.errors && error.error.errors instanceof Array && error.error.errors.length > 0) {
+        error.error.errors.forEach((err: string) => {
+          this.toaster.warning('Client Error', err);
+        });
+      } else {
+        message = error.error?.detail || 'A client error occurred.';
+        this.toaster.warning(`Client Error: ${error.status}`, message);
+      }
     } else if (error.status >= 500) {
       message = `Server Error: ${error.status} - ${error.message}`;
       this.toaster.error('Error', message);
