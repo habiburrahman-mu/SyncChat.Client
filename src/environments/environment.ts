ea9cbdba1b2844ad29396ab560120bb138ac051d
Habibur Rahman <habiburrahman.mu@gmail.com>
2025-12-27 12:24:44 +0600

    fix(environment): revert API base URLs to relative paths

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,6 +2,6 @@ import { Environment } from './environment.model';
 
 export const environment: Environment = {
   production: false,
-  apiBaseUrl: 'https://localhost:5001/api',
-  notificationHubUrl: 'https://localhost:5001/hub/notifications',
+  apiBaseUrl: '/api',
+  notificationHubUrl: '/hub/notifications',
 };
