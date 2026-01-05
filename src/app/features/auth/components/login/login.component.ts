47eb701c309c907c34a344ae22fac610d4f2a842
Habibur Rahman <habiburrahman.mu@gmail.com>
2026-01-05 23:17:32 +0600

    feat(login): integrate Google sign-in button and update login flow

--- src/app/features/auth/components/login/login.component.ts
+++ src/app/features/auth/components/login/login.component.ts
@@ -1,4 +1,4 @@
-import { ChangeDetectionStrategy, Component, DestroyRef, inject, signal } from '@angular/core';
+import { AfterViewInit, ChangeDetectionStrategy, Component, DestroyRef, inject, OnInit, signal } from '@angular/core';
 import { MatCardModule } from '@angular/material/card';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
@@ -13,10 +13,10 @@ import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
 import { MatIconModule } from '@angular/material/icon';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { GoogleIconComponent } from '@shared/components';
-import { CHAT_ROUTES } from '@features/chat/chat.routes';
 import { FEATURE_ROUTE_PATH } from '@core/constants';
 import { AuthService, LocalStorageService } from '@core/services';
 import { LocalStorageKey } from '@core/enums';
+import { environment } from '@environments/environment';
 
 @Component({
   selector: 'chat-login',
@@ -36,7 +36,7 @@ import { LocalStorageKey } from '@core/enums';
   styleUrl: './login.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class LoginComponent {
+export class LoginComponent implements AfterViewInit{
   options: AnimationOptions = {
     path: 'assets/animations/login.json',
     loop: true,
@@ -66,6 +66,22 @@ export class LoginComponent {
 
   isLoginInProgress = signal<boolean>(false);
 
+  ngAfterViewInit(): void {
+    google.accounts.id.initialize({
+      client_id: environment.googleClientId,
+      auto_select: false,
+      cancel_on_tap_outside: true,
+      callback: (response: GoogleCredentialResponse) => {
+        console.log(response);
+      }
+    });
+
+    google.accounts.id.renderButton(
+      document.getElementById('google-btn')!,
+      { theme: 'outline', size: 'medium', text: 'continue_with' }
+    );
+  }
+
   public onSubmit() {
     if (this.form.valid) {
       this.isLoginInProgress.set(true);
@@ -78,7 +94,7 @@ export class LoginComponent {
           next: accessToken => {
             this.storeToken(accessToken);
             this.isLoginInProgress.set(false);
-            this.routeToLChatHome();
+            this.routeToChatHome();
             this._authService.setAuthState(true);
           },
           error: _ => {
@@ -103,7 +119,7 @@ export class LoginComponent {
     // this._localStorageService.setItem(LocalStorageKey.ExpirationInMinutes, response.expirationInMinutes);
   }
 
-  private routeToLChatHome() {
+  private routeToChatHome() {
     this._router.navigate([FEATURE_ROUTE_PATH.Chat]);
   }
 }
