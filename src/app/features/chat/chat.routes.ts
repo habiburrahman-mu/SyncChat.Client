edaee1bf97def69641e4dd307436b9c3de034a54
Habibur Rahman <habiburrahman.mu@gmail.com>
2026-01-11 13:18:29 +0600

    feat: add chat list route

--- src/app/features/chat/chat.routes.ts
+++ src/app/features/chat/chat.routes.ts
@@ -1,5 +1,6 @@
 import { Routes } from "@angular/router";
 
 export const CHAT_ROUTES: Routes = [
-  {path: '', loadComponent: () => import('./components/chat/chat.component').then(c => c.ChatComponent)}
+  {path: '', loadComponent: () => import('./components/chat/chat.component').then(c => c.ChatComponent)},
+  {path: 'list', loadComponent: () => import('./components/chat-sidebar/chat-sidebar.component').then(c => c.ChatSidebarComponent)},
 ];
