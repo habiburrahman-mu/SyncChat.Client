56ed3cc235709cea90e15ceae4dcc336418e5c71
habiburrahman-mu <habiburrahman.mu@gmail.com>
2026-03-15 01:50:22 +0600

    feat: enhance user detail stream to include timestamped avatar URLs

--- src/app/features/chat/services/user-state.service.ts
+++ src/app/features/chat/services/user-state.service.ts
@@ -2,6 +2,7 @@ import { inject, Injectable } from '@angular/core';
 import { UserService } from '.';
 import { rxResource } from '@angular/core/rxjs-interop';
 import { AuthService } from '@core/services';
+import { map } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -11,7 +12,15 @@ export class UserStateService {
   private readonly authService = inject(AuthService);
 
   readonly userResource = rxResource({
-    stream: () => this.userService.getUserDetail(),
+    stream: () => this.userService.getUserDetail().pipe(
+      map(user => {
+        if (user.avatarUrl) {
+          user.avatarUrl = this.addTimeStampToUrl(user.avatarUrl);
+        }
+
+        return user;
+      }),
+    ),
   });
 
   constructor() {
@@ -19,5 +28,11 @@ export class UserStateService {
       .subscribe(isAuthenticated => {
         this.userResource.update(val => val = undefined);
       });
+  }
+
+  private addTimeStampToUrl(url: string): string {
+    const timestamp = new Date().getTime();
+    const separator = url.includes('?') ? '&' : '?';
+    return `${url}${separator}t=${timestamp}`;
   }
 }
