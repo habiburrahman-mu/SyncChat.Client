cdf71b62c1e71a32b3e23a6849ab120a7368f8de
Habibur Rahman <habiburrahman.mu@gmail.com>
2026-04-13 20:12:15 +0600

    feat: add baseHref to angular.json and enable hash location strategy in app config

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,5 +1,5 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import { provideRouter, withHashLocation } from '@angular/router';
 import { routes } from './app.routes';
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { provideLottieOptions } from 'ngx-lottie';
@@ -10,7 +10,7 @@ import { authInterceptor, httpErrorInterceptor, refreshTokenInterceptor } from '
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(routes),
+    provideRouter(routes, withHashLocation()),
     provideHttpClient(withInterceptors([
       authInterceptor,
       refreshTokenInterceptor,
