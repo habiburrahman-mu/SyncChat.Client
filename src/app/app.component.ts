06ebbb1e78c54ee2a1b42a4aa04e0e8d81b382f6
Habibur Rahman <habiburrahman.mu@gmail.com>
2026-04-14 19:40:52 +0600

    refactor: update baseHref in angular.json and clean up app.component files

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,6 @@
 
-import { HttpClient } from '@angular/common/http';
-import { Component, inject, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { map, merge, Observable, startWith, Subject, switchMap } from 'rxjs';
-import { NotificationService } from '@core/services';
 
 @Component({
   selector: 'app-root',
@@ -11,30 +8,6 @@ import { NotificationService } from '@core/services';
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
-export class AppComponent implements OnInit {
+export class AppComponent {
   title = 'SyncChat.Client';
-
-  forecasts$!: Observable<any[]>;
-  _buttonClicked$ = new Subject<void>;
-
-  private apiUrl = 'https://localhost:5001/weatherforecasts';  // Adjust your API URL
-
-  private _httpClient = inject(HttpClient);
-  private readonly notificationService = inject(NotificationService);
-
-  ngOnInit(): void {
-    this.forecasts$ = this._buttonClicked$.pipe(
-      startWith(undefined),
-      switchMap(() => this.getForecasts())
-    );
-  }
-
-  getForecasts(): Observable<any[]> {
-    return this._httpClient.get<any[]>(this.apiUrl);
-  }
-
-  onClick() {
-    this._buttonClicked$.next();
-    console.log("Test");
-  }
 }
